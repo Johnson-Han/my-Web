13c38519aa17a62040523a1a8dd891ce6ebf7ac8
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-03-16 14:54:15 +0800

    begin angular test

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/ng-test', function (req, res, next) {
+  res.render('ng-test', { title: 'ng-test' });
+});
 module.exports = router;
